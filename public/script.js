7bc4a20dbb26ebf89ef95b73aa9a9f30c7ad5b60
mpara30 <para.mihai99@gmail.com>
2021-10-20 18:31:33 +0300

    starting version

---
+++ public/script.js
@@ -0,0 +1,90 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const questionContainerElement = document.getElementById('question-container')
+
+var shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffledQuestions.length > currentQuestionIndex + 1){
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if(correct){
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element, correct){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+var questions =  [
+    {
+        question: 'It is done this app?',
+        answers: [
+            { text: 'Nope', correct: true },
+            { text: 'Yes, ofc', correct: false},
+            { text: 'test 1', correct: false},
+            { text: 'test 2', correct: false}
+        ] 
+    }
+] 
---
+++ server.js
@@ -0,0 +1,21 @@
+//Load node
+var express = require('express');
+const ejs = require('ejs');
+
+//Initialize Express
+var app = express();
+
+//Render static files
+app.use(express.static('public'));
+
+//Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//Port website will run
+app.listen(8080);
+
+// *** GET Routes - display pages ***
+// Root Route
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
